8d163355ffcd047cf2a22ebdabfac2696b11fa76
Fred Nyakagwa <51993500+nyakagwafred@users.noreply.github.com>
2020-03-31 20:27:06 +0300

    modified:   scripts/scripts.js / Added static image to the DOM

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -7,13 +7,15 @@ const pressureInput = document.getElementById('pressure');
 const windspeedInput = document.getElementById('windspeed');
 const humidityInput = document.getElementById('humidity');
 const temperatureInput = document.getElementById('temperature');
+//const staticImage = document.getElementById('staticMap');
 
 let apiRequest = new XMLHttpRequest();
 
 cityForm.addEventListener('submit', ($event) => {
-    $event.preventDefault();
+   $event.preventDefault();
     const chosenCity = cityInput.value;
-    apiRequest.open('GET', 'https://api.openweathermap.org/data/2.5/weather?q=' + chosenCity + '&APPID=b34fddd3dae4a2eb0ad363b62f98ba1e');
+    document.getElementById('staticMap').src = 'https://www.mapquestapi.com/staticmap/v5/map?key=xA1StDfcjv6G8mkGFjjERZXtMGBAvGGc&center= ' + chosenCity + '&size=300,200@2x';
+    apiRequest.open('GET', 'https://api.openweathermap.org/data/2.5/weather?q=' + chosenCity + '&APPID=b34fddd3dae4a2eb0ad363b62f98ba1e&units=metric');
     apiRequest.send();
 
 });
@@ -21,15 +23,19 @@ cityForm.addEventListener('submit', ($event) => {
 apiRequest.onreadystatechange = () => {
     if (apiRequest.readyState === 4) {
         if (apiRequest.status === 404) {
-            return updateTxt.textContent = 'City not found';
+            return updateTxt.textContent = 'City not found', pressureInput.textContent = '0',
+            windspeedInput.textContent = '0', humidityInput.textContent = '0', temperatureInput.textContent = '0';           
         }
-        const response = JSON.parse(apiRequest.response);
-        updateTxt.textContent = 'Current Weather Conditions in ' + response.name + ' is ' + response.weather[0].main + ' .';
-        pressureInput.textContent = 'Pressure :  ' + response.main.pressure + '.';
-        windspeedInput.textContent = 'Wind Speed :  ' + response.wind.speed + '.';
-        humidityInput.textContent = 'Humidity :  ' + response.main.humidity + '.';
-        temperatureInput.textContent = 'Feels Like :  ' + response.main.feels_like + '. Fahrenheit';
 
+        else {
+            const response = JSON.parse(apiRequest.response);
+            updateTxt.textContent = 'Current Weather Conditions in ' + response.name + ' is ' + response.weather[0].main + ' .';
+            pressureInput.textContent = 'Pressure :  ' + response.main.pressure + '.';
+            windspeedInput.textContent = 'Wind Speed :  ' + response.wind.speed + '.';
+            humidityInput.textContent = 'Humidity :  ' + response.main.humidity + '.';
+            temperatureInput.textContent = 'Feels Like :  ' + response.main.feels_like + '. Celcius';
+
+        }
     }
 }
 
